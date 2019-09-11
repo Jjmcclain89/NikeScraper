6540dabc740abde8ae89bbd362c527f526ed7695
Jjmcclain <31944090+Jjmcclain-Uiowa@users.noreply.github.com>
2019-09-11 18:10:41 -0500

    styling

--- components/Page.js
+++ components/Page.js
@@ -1,16 +1,52 @@
 import { useEffect, useState } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 import { ShoeProvider } from './ShoeContext';
 import { getShoeData } from '../lib/lib.js';
+
+const theme = {
+	red: '#FF0000',
+	black: '#393939',
+	grey: '#3A3A3A',
+	lightgrey: '#E1E1E1',
+	offWhite: '#EDEDED',
+	maxWidth: '1000px',
+	bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)'
+};
+
+const StyledPage = styled.div`
+	color: ${props => props.theme.black};
+`;
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    background: ${theme.offWhite};
+    box-sizing: border-box;
+    font-size: 16px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1rem;
+    line-height: 2;
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;
 
 const useShoes = () => {
 	const [shoes, setShoes] = useState([]);
 
 	useEffect(() => {
-        const getData = async () => {
-            setShoes(await getShoeData());
-        }
-        getData();
-        console.log('Mounting or Updating');
+		const getData = async () => {
+			setShoes(await getShoeData());
+		};
+		getData();
+		console.log('Mounting or Updating');
 	}, []);
 
 	return shoes;
@@ -19,9 +55,12 @@ const useShoes = () => {
 const Page = ({ children }) => {
 	const shoes = useShoes();
 	return (
-		<ShoeProvider value={shoes}>
-			<div className="Page">{children}</div>
-		</ShoeProvider>
+		<>
+			<GlobalStyle />
+			<ShoeProvider value={shoes}>
+				<StyledPage>{children}</StyledPage>
+			</ShoeProvider>
+		</>
 	);
 };
 
